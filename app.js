b6c36f5aaff4d3fa3f2a16328f9f723a9b83eac8
CoolShivas <shivas2710cool00@gmail.com>
2025-08-27 20:45:37 +0530

    Useful things of ExpressJS.

--- app.js
+++ app.js
@@ -1,32 +1,5 @@
 // const express = require("express");
 import express from "express";
-// // // Now, stop or kill the previous server by opening Terminal and run the command (ctrl + c).
-// // // And, again run the command  node app.js to start the sever. We will get the Output on Terminal as server running at 3000
-// // // Now, open any Browser and enter in URL as localhost:3000 or http://localhost:3000/
-// // // We will get the Output on Browser Display as :- Hello, in the World of SHIVAJI
-
-// // // Now, open any Browser's Network Tab you will see localhost
-// // // CLick on localhost you will see Headers, Preview, Response, Initiator, Timining
-// // // CLick on Headers you will see :-
-/**
- * 
- * 
-Request URL              http://localhost:3000/
-Request Method              GET
-Status Code                 304 Not Modified
-Remote Address              [::1]:3000
-Referrer Policy             strict-origin-when-cross-origin
- * 
- * 
- * 
-connection                  keep-alive
-date                        Wed, 27 Aug 2025 14:33:46 GMT
-etag                        W/"1e-8WMfZX3zLP2yYydKJsoF+YJkFww"
-keep-alive                  timeout=5
-x-powered-by                Express
- * 
- */
-// // //
 
 // // // Created instance for express.
 const server = express();
@@ -36,7 +9,37 @@ const PORT = 3000;
 
 // // // Formation of home page showing the response.
 server.get("/", (request, response) => {
-  response.send("Hello, in the World of SHIVAJI");
+  response.send("<center>Hello, in the World of SHIVAJI</center>");
+  // // // Automatically, ExpressJS understand itself most of the things such as (Request Method GET; Status Code 200 OK;content-type text/html; charset=utf-8;). Previously, we have to defined this things in NodeJS.
+  // // // Now, stop or kill the previous server by opening Terminal and run the command (ctrl + c).
+  // // // And, again run the command  node app.js to start the sever. We will get the Output on Terminal as server running at 3000
+  // // // Now, open any Browser and enter in URL as localhost:3000 or http://localhost:3000/
+  // // // We will get the Output on Browser Display as :- Hello, in the World of SHIVAJI
+
+  // // // Now, open any Browser's Network Tab you will see localhost
+  // // // CLick on localhost you will see Headers, Preview, Response, Initiator, Timining
+  // // // CLick on Headers you will see :-
+  /**
+ * 
+ * 
+Request URL                 http://localhost:3000/
+Request Method              GET
+Status Code                 200 OK
+Remote Address              [::1]:3000
+Referrer Policy             strict-origin-when-cross-origin
+ * 
+ * 
+ * 
+connection                  keep-alive
+content-length              47
+content-type                text/html; charset=utf-8
+date                        Wed, 27 Aug 2025 14:33:46 GMT
+etag                        W/"1e-8WMfZX3zLP2yYydKJsoF+YJkFww"
+keep-alive                  timeout=5
+x-powered-by                Express
+ * 
+ */
+  // // //
 });
 
 // // // Running the server or port at define number.
