f80d45071d2059a346b056a9c5659a73d1b0d463
CoolShivas <shivas2710cool00@gmail.com>
2025-08-27 22:02:38 +0530

    Formation of multiple pages in expressJS.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // const express = require("express");
-import express from "express";
+import express, { response } from "express";
 
 // // // Created instance for express.
 const server = express();
@@ -7,39 +7,33 @@ const server = express();
 // // // Mentioning of port number or defining it.
 const PORT = 3000;
 
-// // // Formation of home page showing the response.
+// // // Formation of HOME PAGE showing the response.
 server.get("/", (request, response) => {
   response.send("<center>Hello, in the World of SHIVAJI</center>");
-  // // // Automatically, ExpressJS understand itself most of the things such as (Request Method GET; Status Code 200 OK;content-type text/html; charset=utf-8;). Previously, we have to defined this things in NodeJS.
-  // // // Now, stop or kill the previous server by opening Terminal and run the command (ctrl + c).
-  // // // And, again run the command  node app.js to start the sever. We will get the Output on Terminal as server running at 3000
-  // // // Now, open any Browser and enter in URL as localhost:3000 or http://localhost:3000/
-  // // // We will get the Output on Browser Display as :- Hello, in the World of SHIVAJI
+});
 
-  // // // Now, open any Browser's Network Tab you will see localhost
-  // // // CLick on localhost you will see Headers, Preview, Response, Initiator, Timining
-  // // // CLick on Headers you will see :-
-  /**
- * 
- * 
-Request URL                 http://localhost:3000/
-Request Method              GET
-Status Code                 200 OK
-Remote Address              [::1]:3000
-Referrer Policy             strict-origin-when-cross-origin
- * 
- * 
- * 
-connection                  keep-alive
-content-length              47
-content-type                text/html; charset=utf-8
-date                        Wed, 27 Aug 2025 14:33:46 GMT
-etag                        W/"1e-8WMfZX3zLP2yYydKJsoF+YJkFww"
-keep-alive                  timeout=5
-x-powered-by                Express
- * 
- */
-  // // //
+// // // Formation of ABOUT PAGE showing the response.
+server.get("/about", (req, res) => {
+  res.send("<h1>This is the About Page.</h1>");
+});
+
+// // // Formation of CONTACT PAGE showing the response.
+server.get("/contact", (req, res) => {
+  return res.send(`<div class="container">
+      <h1>URL Shortner</h1>
+      <form id="shorten-form">
+        <div>
+          <label for="url">Enter URL :-</label>
+          <input type="text" name="url" id="url" required />
+        </div>
+        <div>
+          <label for="url">Enter Short-Code :-</label>
+          <input type="text" name="short" id="short" required />
+        </div>
+        <button type="submit">Shorten</button>
+      </form>
+      <h2>Shortened URLs</h2>
+    </div>`);
 });
 
 // // // Running the server or port at define number.
