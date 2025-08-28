65e4ea963762cd5a567292ac0543d7f62da85992
CoolShivas <shivas2710cool00@gmail.com>
2025-08-28 12:39:33 +0530

    Installation of zod and using its validation by making the port number as string.

--- environ.js
+++ environ.js
@@ -1,1 +1,7 @@
-export const PORT = isNaN(process.env.PORT) ? 3000 : parseInt(process.env.PORT);
+// export const PORT = isNaN(process.env.PORT) ? 3000 : parseInt(process.env.PORT);
+
+import { z } from "zod";
+
+const portSchema = z.coerce.number().min(1).max(65535).default(9000);
+
+export const PORT = portSchema.parse(process.env.PORT);
